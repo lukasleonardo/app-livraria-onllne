38eea14b6c5e097bf42c8f3d836c70ea2866352f
Lucas Henrique da Costa Leonardo <lucas.leonardo@nemu.com.br>
2025-01-29 09:04:02 -0300

    layout container

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,9 +21,9 @@ export default function RootLayout({children,}:{children: React.ReactNode;}) {
     <html lang="en" >
       <body className={`${inter.className} themed-bg`} >
       <Providers>
-      <Navigation/>  
-        {children}
-        </Providers>
+        <Navigation/>  
+        <main className="container mx-auto px-4 py-8">{children}</main>
+      </Providers>
       </body>
     </html>
   );
